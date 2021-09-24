8cc0ca82664eeb8e9987f18c18eda5d185459e3b
GeonHui Yoon <rkawk3316@gmail.com>
2021-09-24 08:27:45 +0300

    Added original image display and removed unnecessary code

--- sizeCheck.py
+++ sizeCheck.py
@@ -1,7 +1,4 @@
 import cv2
-
-
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,23 +11,23 @@ class SizeCheck:
 
     def checkLargest(img):
 
-        blockSize =9
+        blockSize =51
         C = 5
 
         blur = cv2.GaussianBlur(img, (3, 3), 0)
         gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        thresholdMean = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,blockSize, C)
+
+        #thresholdMean = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,blockSize, C)
         thresholdGaussian = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, blockSize, C)
         final = cv2.cvtColor(thresholdGaussian, cv2.COLOR_BGR2RGB)
         #cv2.imshow('Thresh-Mean', thresholdMean)
-
 
         # Perform connected component labeling
         n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresholdGaussian, connectivity=4)
 
         # Create false color image and color background black
         colors = np.random.randint(0, 255, size=(n_labels, 3), dtype=np.uint8)
-        colors[0] = [0, 0, 0]  # for cosmetic reason we want the background black
+        colors[0] = [0, 0, 0]
         false_colors = colors[labels]
 
         # Label area of each polygon
@@ -46,6 +43,7 @@ class SizeCheck:
         #    plt.imshow(v)
         #    plt.xticks([]), plt.yticks([])
         #plt.show()
+        cv2.imshow('Original sample', img)
         cv2.imshow('Area',false_colors_area)
         cv2.waitKey()
         cv2.destroyAllWindows()
